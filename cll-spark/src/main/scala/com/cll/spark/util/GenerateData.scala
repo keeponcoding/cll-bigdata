2bbb3f271ef4b7dcee333c6cd68c94600365da22
cll <chenleilei_92@163.com>
2020-02-05 22:03:51 +0800

    cll-spark

--- cll-spark/src/main/scala/com/cll/spark/util/GenerateData.scala
+++ cll-spark/src/main/scala/com/cll/spark/util/GenerateData.scala
@@ -1,7 +1,11 @@
 package com.cll.spark.util
 
-import java.io.{File, PrintWriter}
+import java.util.Properties
 
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.slf4j.LoggerFactory
+
+import scala.util.Random
 
 /**
   * @ClassName GenerateData
@@ -12,13 +16,30 @@ import java.io.{File, PrintWriter}
   **/
 object GenerateData {
 
+  val log = LoggerFactory.getLogger(this.getClass.getSimpleName)
+
   def main(args: Array[String]): Unit = {
 
-    val writer = new PrintWriter(new File("cll-spark/file/a.txt"))
+    val props = new Properties();
+    props.put("bootstrap.servers", "hadoop000:9092,hadoop000:9093,hadoop000:9094");
+    props.put("acks", "all");
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-    writer.write("hello,scala,spark")
+    val producer = new KafkaProducer[String,String](props);
 
-    writer.close()
+    for (i <- 0 to 10) {
+      Thread.sleep(100)
+
+      val word = String.valueOf((new Random().nextInt(6) + 'a').toChar)
+      val part = i % 3
+
+      log.error("生成数据：" + word)
+
+      producer.send(new ProducerRecord[String, String]("test", null, word))
+    }
+
+    producer.close();
 
   }
 
